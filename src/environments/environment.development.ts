5985ace1ef48685a0543ee57b370124981a6724d
Sarah <s.fawzy97@yahoo.com>
2026-07-16 15:17:35 +0300

    generate environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+        production:true,
+  supabaseUrl: 'https://kuncjqsalpsqfohvzvfa.supabase.co',
+  supabasePublishableKey: 'sb_publishable_tqKPn6YjijPzm3AIbEZ9pw_1VguOcXw',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+     production: false,
+  supabaseUrl: 'https://kuncjqsalpsqfohvzvfa.supabase.co',
+  supabasePublishableKey: 'sb_publishable_tqKPn6YjijPzm3AIbEZ9pw_1VguOcXw',
+
+};
