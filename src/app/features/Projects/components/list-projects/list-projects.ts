1a30f992189f7b76718af40fcd87254f3fce7c0f
Sarah <s.fawzy97@yahoo.com>
2026-07-28 19:21:42 +0300

    .

--- src/app/features/Projects/components/list-projects/list-projects.ts
+++ src/app/features/Projects/components/list-projects/list-projects.ts
@@ -6,49 +6,46 @@ import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-list-projects',
-  imports: [RouterLink,DatePipe],
+  imports: [RouterLink, DatePipe],
   templateUrl: './list-projects.html',
   styleUrl: './list-projects.css',
 })
 export class ListProjects {
+  projects = signal<Project[]>([]);
+  myDate: Date = new Date();
+  projeService = inject(Projects);
+  router = inject(Router);
+  errorDisplayed = signal(false);
+  isLoading = signal(true);
 
-projects = signal<Project[]>([]);
- myDate: Date = new Date(); 
-   projeService=inject(Projects)
- router=inject(Router)
- errorDisplayed=signal(false)
- isLoading=signal(true)
+  ngOnInit() {
+    this.getProjects();
+  }
 
-   ngOnInit(){
-    this.getProjects()
-   }
-
-   getProjects(){
+  getProjects() {
     this.projeService.getProject().subscribe({
-      next:(res)=>{
-        console.log(res)
-        this.projects.set(res)
-this.isLoading.set(false)
-
-      },error:(err)=>{
-        console.log(err)
-        if(err.status===401){
-this.isLoading.set(false)
-    this.router.navigateByUrl('/login');
-        }else
-          {this.errorDisplayed.set(true)}
-      },complete:()=> {
-// this.isLoading.set(false)
-        
+      next: (res) => {
+        console.log(res);
+        this.projects.set(res);
+        this.isLoading.set(false);
       },
+      error: (err) => {
+        console.log(err);
+        if (err.status === 401) {
+          this.isLoading.set(false);
+          this.router.navigateByUrl('/login');
+        } else {
+          this.errorDisplayed.set(true);
+        }
+      },
+      complete: () => {
+        // this.isLoading.set(false)
+      },
+    });
+  }
 
-    })
-   }
-
-   getOneProj(project:Project){
-    this.projeService.selectedProject.set(project)
-    this.router.navigate(['project/'+project.id+'/edit'])
-   }
+  getOneProj(project: Project) {
+    this.projeService.selectedProject.set(project);
+    this.router.navigate(['project/' + project.id + '/edit']);
+  }
 }
-
-
