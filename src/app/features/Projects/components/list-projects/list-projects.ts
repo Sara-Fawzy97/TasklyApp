e31b02e9b5b4fa57888cf24b78fb6280549f5af9
Sarah <s.fawzy97@yahoo.com>
2026-07-28 19:26:11 +0300

    format doc

--- src/app/features/Projects/components/list-projects/list-projects.ts
+++ src/app/features/Projects/components/list-projects/list-projects.ts
@@ -1,9 +1,9 @@
-import { Component, inject, signal, AfterViewInit,ElementRef,ViewChild } from '@angular/core';
+import { Component, inject, signal, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { Projects } from '../../services/projects';
 import { Project } from '../../models/project';
 import { Router, RouterLink } from '@angular/router';
 import { DatePipe } from '@angular/common';
-import { Pagination } from "../../../../shared/components/pagination/pagination";
+import { Pagination } from '../../../../shared/components/pagination/pagination';
 
 @Component({
   selector: 'app-list-projects',
@@ -11,7 +11,7 @@ import { Pagination } from "../../../../shared/components/pagination/pagination"
   templateUrl: './list-projects.html',
   styleUrl: './list-projects.css',
 })
-export class ListProjects implements AfterViewInit{
+export class ListProjects implements AfterViewInit {
   projects = signal<Project[]>([]);
   myDate: Date = new Date();
   projeService = inject(Projects);
@@ -19,10 +19,10 @@ export class ListProjects implements AfterViewInit{
   errorDisplayed = signal(false);
   isLoading = signal(false);
 
-  pageSize=10
-  currentPage=1
-  totalItems=0
-  totalPages=1
+  pageSize = 10;
+  currentPage = 1;
+  totalItems = 0;
+  totalPages = 1;
 
   @ViewChild('loadMoreTrigger')
   loadMoreTrigger!: ElementRef;
@@ -33,37 +33,32 @@ export class ListProjects implements AfterViewInit{
 
   ngOnInit() {
     // this.getProjects();\
-      this.hasMore = true;
-    this.paginator()
+    this.hasMore = true;
+    this.paginator();
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     // console.log(this.loadMoreTrigger);
-      // console.log('AfterViewInit');
-     this.observer = new IntersectionObserver(entries => {
-
-    if (entries[0].isIntersecting) {
-//  console.log('Visible');
-      this.loadMore();
-
-    }
-  });
+    // console.log('AfterViewInit');
+    this.observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        //  console.log('Visible');
+        this.loadMore();
+      }
+    });
     this.observer.observe(this.loadMoreTrigger.nativeElement);
   }
 
-
   //mobile only
-loadMore() {
+  loadMore() {
+    if (this.isLoading() || !this.hasMore) return;
 
-  if (this.isLoading() || !this.hasMore) return;
+    this.isLoading.set(true);
 
-  this.isLoading.set( true);
+    this.currentPage++;
 
-  this.currentPage++;
-
-  this.paginator(true);
-
-}
+    this.paginator(true);
+  }
 
   getProjects() {
     this.projeService.getProject().subscribe({
@@ -92,39 +87,35 @@ loadMore() {
     this.router.navigate(['project/' + project.id + '/edit']);
   }
 
-paginator( append=false){
-        this.isLoading.set(false);
+  paginator(append = false) {
+    this.isLoading.set(false);
 
-  const offset=(this.currentPage-1)*this.pageSize
-  this.projeService.getPaginatedProjects(this.pageSize,offset).subscribe({
-    next:(res)=>{
-      console.log(res)
-      if(append){
-        this.projects.set( [
-  ...this.projects(),
-  ...(res.body ?? [])
-]);
-      }else this.projects.set(res.body??[])
+    const offset = (this.currentPage - 1) * this.pageSize;
+    this.projeService.getPaginatedProjects(this.pageSize, offset).subscribe({
+      next: (res) => {
+        console.log(res);
+        if (append) {
+          this.projects.set([...this.projects(), ...(res.body ?? [])]);
+        } else this.projects.set(res.body ?? []);
 
         this.isLoading.set(false);
 
-         if ((res.body?.length ?? 0) < this.pageSize) {
+        if ((res.body?.length ?? 0) < this.pageSize) {
+          this.hasMore = false;
+        }
+        const ContentRange = res.headers.get('Content-Range');
+        // console.log(ContentRange)
+        if (ContentRange) {
+          this.totalItems = Number(ContentRange.split('/')[1]);
+          this.totalPages = Math.ceil(this.totalItems / this.pageSize);
 
-            this.hasMore = false;
-
-          }
-      const ContentRange=res.headers.get('Content-Range')
-      // console.log(ContentRange)
-      if(ContentRange){
-        this.totalItems=Number(ContentRange.split('/')[1]);
-        this.totalPages=Math.ceil(this.totalItems/this.pageSize)
-
-//         this.pages = Array.from(
-//   { length: this.totalPages },
-//   (_, i) => i + 1
-// );
-      }
-    }, error: (err) => {
+          //         this.pages = Array.from(
+          //   { length: this.totalPages },
+          //   (_, i) => i + 1
+          // );
+        }
+      },
+      error: (err) => {
         console.log(err);
         if (err.status === 401) {
           this.isLoading.set(false);
@@ -133,17 +124,13 @@ paginator( append=false){
           this.errorDisplayed.set(true);
         }
       },
-  })
-}
+    });
+  }
 
-//for desktop
-changePage(page:number) {
+  //for desktop
+  changePage(page: number) {
+    this.currentPage = page;
 
-  this.currentPage = page;
-
-  this.paginator(false);
-
-}
-
-
+    this.paginator(false);
+  }
 }
