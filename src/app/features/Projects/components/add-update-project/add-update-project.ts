bab6d11d8faa6e8c41bf362219a7ea082853d8f4
Sarah <s.fawzy97@yahoo.com>
2026-07-28 05:52:33 +0300

    update name validation ,add toastr and customize btn

--- src/app/features/Projects/components/add-update-project/add-update-project.ts
+++ src/app/features/Projects/components/add-update-project/add-update-project.ts
@@ -1,7 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Projects } from '../../services/projects';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Toastr } from '../../../../core/services/toastr';
 
 @Component({
   selector: 'app-add-update-project',
@@ -10,63 +11,81 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './add-update-project.css',
 })
 export class AddUpdateProject {
+  projService = inject(Projects);
+  toastrService = inject(Toastr);
+  router = inject(Router);
+  route = inject(ActivatedRoute);
 
-  projService=inject(Projects)
-errorMsg=signal("")
-project=this.projService.selectedProject();
-// route=inject(Router)
- route = inject(ActivatedRoute);
-projectId=0
+  errorMsg = signal('');
+  project = this.projService.selectedProject();
+  projectId = 0;
+  isedit = signal(false);
 
-ngOnInit(){
-  console.log(this.project)
-  this.projectId=this.route.snapshot.params['id']
-  if(this.projectId){
-    this.isedit.set(true)
-  this.getProjData()
 
+  ngOnInit() {
+   this.checkPageName()
   }
+checkPageName(){
+ this.projectId = this.route.snapshot.params['id'];
+    if (this.projectId) {
+      this.isedit.set(true);
+      this.getProjData();
+    }
 }
 
- createProj=new FormGroup({
-   name: new FormControl('',[Validators.required,Validators.min(3),Validators.pattern(/^(?=.{3,100}$)[\p{L}]+(?: [\p{L}]+)*$/u)]),
-   description:new FormControl('',[Validators.max(500)])
- })
+  createProj = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.pattern(/^(?=.{3,100}$)[\p{L}\d\s\-_.()&]+$/u),
+    ]),
+    description: new FormControl('', [Validators.maxLength(500)]),
+  });
 
- addNewProj(data:FormGroup){
- this.projService.addNew(data.value).subscribe({
-      next:(res)=>{
-        console.log(res)
-
-      }, error:(err)=>{
-        this.errorMsg=err.error.message
-        console.log(err)
-      },complete:()=>{
-         this.createProj.reset()
+  addNewProj(data: FormGroup) {
+    this.projService.addNew(data.value).subscribe({
+      next: (res) => {
+        this.toastrService.success('Your project is added successfully', 'top-right');
+        console.log(res);
+      },
+      error: (err) => {
+        this.errorMsg = err.error.message;
+        this.toastrService.error(err.error.message, 'top-right');
+      },
+      complete: () => {
+        this.createProj.reset();
+        this.router.navigateByUrl('/project');
+      },
+    });
   }
- })
-  // console.log(data)
- }
 
- getProjData(){
-  this.createProj.patchValue({
-    name:this.project?.name,
-    description:this.project?.description
-  })
- }
-isedit=signal(false)
- updateProj(data:FormGroup){
-  this.projService.updateProject(data.value,this.projectId).subscribe({
-    next:(res)=>{
-      console.log(res)
+  getProjData() {
+    this.createProj.patchValue({
+      name: this.project?.name,
+      description: this.project?.description,
+    });
+  }
+  updateProj(data: FormGroup) {
+    this.projService.updateProject(data.value, this.projectId).subscribe({
+      next: (res) => {
+        this.toastrService.success('Your project is updated successfully', 'top-right');
+
+        console.log(res);
+      },error:(err)=>{
+        this.toastrService.error(err.error.message, 'top-right');
+      }, complete:() =>{
+          this.createProj.reset();
+        this.router.navigateByUrl('/project');
+      },
+    });
+  }
+
+
+  onSubmit(data: FormGroup) {
+    if (this.projectId) {
+      this.updateProj(data);
+    } else {
+      this.addNewProj(data);
     }
-  })
- }
- onSubmit(data:FormGroup){
-  if(this.projectId){
-this.updateProj(data)
-  }else { this.addNewProj(data)}
- }
-
- 
+  }
 }
