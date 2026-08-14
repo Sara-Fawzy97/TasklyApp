3a31061bce1ab1f1e5014ab3a9d53408b06b9694
Sarah <s.fawzy97@yahoo.com>
2026-08-14 23:26:51 +0300

    Merge tm-22 tasks board view to staging

--- src/app/features/Tasks/components/all-tasks/all-tasks.ts
+++ src/app/features/Tasks/components/all-tasks/all-tasks.ts
@@ -1,32 +1,62 @@
-import { Component, DestroyRef, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { TasksService } from '../../services/tasks-service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Task } from '../../models/ITask';
 
 @Component({
   selector: 'app-all-tasks',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './all-tasks.html',
   styleUrl: './all-tasks.css',
 })
 export class AllTasks {
 
-    private route = inject(ActivatedRoute);
-  projectId = '';
-  taskService = inject(TasksService);
-  private destroyRef = inject(DestroyRef);
+statuses=['TO_DO','IN_PROGRESS','BLOCKED','IN_REVIEW','READY_FOR QA','REOPENED','READY_FOR_PRODUCTION','DONE']
+projectId=''
+// tasks=signal<Task[]>([])
+  myDate: Date = new Date();
+tasksByStatus = signal<Record<string, Task[]>>({});
+
+   route = inject(ActivatedRoute);
+tasksService=inject(TasksService)
+
+ngOnInit(){
+  this.projectId = this.route.snapshot.params['id'];
+  this.statuses.forEach(s=>{
+    this.getTasks(s)
+  })
+
+}
+
+value = '';
+  getInitials(name: string) {
+    if (!name) return '';
+
+    const words = name.trim().split('');
+
+    if (words.length === 1) {
+      return words[0].substring(0, 2).toUpperCase();
+    }
+
+    return (words[0][0] + words[1][0]).toUpperCase();
+  }
+
+gettasksByStatus(status: string): Task[] {
+  return this.tasksByStatus()[status] ?? [];
+}
+
+getTasks(status:string){
   
-
-  ngOnInit(){
-    this.projectId = this.route.snapshot.paramMap.get('id')!;
-this.getAllTasks()
-  }
-
-  getAllTasks(){
-this.taskService.getProjTasks(this.projectId).pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
+ this.tasksService.getProjTasks(this.projectId,status).subscribe({
   next:(res)=>{
-console.log(res)
-  }
+   this.tasksByStatus.update(current => ({
+        ...current,
+        [status]: res
+      }));
+   }
 })
-  }
+}
+
+
 }
