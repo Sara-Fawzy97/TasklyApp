019645cd454649631b21546603971c4cc33bbb0f
Sarah <s.fawzy97@yahoo.com>
2026-08-28 17:49:47 +0300

    edit pagination style

--- src/app/shared/components/pagination/pagination.ts
+++ src/app/shared/components/pagination/pagination.ts
@@ -16,16 +16,21 @@ export class Pagination {
   pageNum=output<number>()
   
 
-   get pages() :(string|number)[] {
-    if(this.totalPages()<=4){
+   get pages() :(string |number)[] {
+
+    if(this.totalPages()<=5){
     return Array.from(
-      { length: this.totalPages()?? 0 },
+      { length: this.totalPages() },
       (_, i) => i + 1
     );}
 
-    if(this.currentPage()<=2 || this.currentPage()>= this.totalPages()){
-    return [1,2,'...',this.totalPages()]
-    }else  return [1,2,'...',this.currentPage(),'...',this.totalPages()]
+    if(this.currentPage()<=3 ){
+    return [1,2,3,4,'...',this.totalPages()]
+    }
+      if( this.currentPage() >= this.totalPages() - 2){
+           return [1, '...', this.totalPages() - 3, this.totalPages() - 2, this.totalPages() - 1,this.totalPages()];
+      }
+   return [1,'...',this.currentPage() - 1, this.currentPage(),this.currentPage() + 1,'...',this.totalPages()];
   }
 
 goToPage(page: number|string) {
